883cbd1977b5cd2857dccfc02c6d659935cf7131
GEZUS95 <ceesgribnau@hotmail.com>
2022-04-21 16:21:33 +0200

    added api endpoints and changed the array to an arraylist

--- src/main/java/nl/ceesgribnau/javaAdvanced/service/ReviewService.java
+++ src/main/java/nl/ceesgribnau/javaAdvanced/service/ReviewService.java
@@ -3,22 +3,32 @@ package nl.ceesgribnau.javaAdvanced.service;
 import nl.ceesgribnau.javaAdvanced.model.Review;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ReviewService {
-    private List<Review> reviews;
+    private List<Review> reviews = new ArrayList<>();
 
     public ReviewService() {
-        this.reviews = Arrays.asList(
-                new Review(1, "Review 1", "Dit is review 1", 5),
-                new Review(2, "Review 2", "Dit is review 2", 2),
-                new Review(3, "Review 3", "Dit is review 3", 4)
-        );
+        this.reviews.add(new Review(1, "Review 1", "Dit is review 1", 5));
+        this.reviews.add(new Review(2, "Review 2", "Dit is review 2", 2));
+        this.reviews.add(new Review(3, "Review 3", "Dit is review 3", 4));
     }
 
-    public List<Review> getAllRieviews() {
+    public List<Review> getAllReviews() {
         return reviews;
+    }
+
+    public Review getReviewById(int id) {
+        return reviews.get(id - 1);
+    }
+
+    public void deleteReviewById(int id) {
+        reviews.remove(id - 1);
+    }
+
+    public void createReview(Review review) {
+        reviews.add(review);
     }
 }
