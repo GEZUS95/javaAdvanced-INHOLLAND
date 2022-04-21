8d97939b55e1a3d66f1d82ac5f2cbb6e935cd2cb
GEZUS95 <ceesgribnau@hotmail.com>
2022-04-21 12:36:56 +0200

    added model

---
+++ src/main/java/nl/ceesgribnau/javaAdvanced/model/review.java
@@ -0,0 +1,50 @@
+package nl.ceesgribnau.javaAdvanced.model;
+
+public class review {
+    private long id;
+    private String title;
+    private String description;
+    private int rating;
+
+    public review() {
+    }
+
+    public review(long id, String title, String description, int rating) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+}
